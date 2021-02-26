834787acba6ac5535a5b169c233e3bfcc71a7f2c
Michael Clark <michaeljclark@mac.com>
2021-02-27 12:43:00 +1300

    crefl-tests: tie down remaining properties in t3 with asserts

--- test/t3.c
+++ test/t3.c
@@ -9,9 +9,7 @@
 #include "cdump.h"
 #include "cfileio.h"
 
-#define DB_DIR "build/tmp/"
-
-/* adjacent-structs-1.h.refl */
+#define DB_DIR "build/tmp/" /* adjacent-structs-1.h.refl */
 
 void t3()
 {
@@ -37,6 +35,12 @@ void t3()
     assert(_fields);
     crefl_struct_fields(_types[0], _fields, &nfields);
     assert(nfields == 6);
+    assert(crefl_is_field(_fields[0]));
+    assert(crefl_is_field(_fields[1]));
+    assert(crefl_is_field(_fields[2]));
+    assert(crefl_is_field(_fields[3]));
+    assert(crefl_is_field(_fields[4]));
+    assert(crefl_is_field(_fields[5]));
     assert(crefl_decl_tag(_fields[0]) == _decl_field);
     assert(crefl_decl_tag(_fields[1]) == _decl_field);
     assert(crefl_decl_tag(_fields[2]) == _decl_field);
@@ -49,6 +53,19 @@ void t3()
     assert(strcmp(crefl_decl_name(_fields[3]), "h") == 0);
     assert(strcmp(crefl_decl_name(_fields[4]), "hu") == 0);
     assert(strcmp(crefl_decl_name(_fields[5]), "w") == 0);
+    assert(crefl_type_width(crefl_field_type(_fields[0])) == 1);
+    assert(crefl_type_width(crefl_field_type(_fields[1])) == 8);
+    assert(crefl_type_width(crefl_field_type(_fields[2])) == 8);
+    assert(crefl_type_width(crefl_field_type(_fields[3])) == 16);
+    assert(crefl_type_width(crefl_field_type(_fields[4])) == 16);
+    assert(crefl_type_width(crefl_field_type(_fields[5])) == 32);
+    assert(crefl_type_width(crefl_field_type(_fields[0])) == 1);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[0])) & _sint) == _sint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[1])) & _sint) == _sint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[2])) & _uint) == _uint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[3])) & _sint) == _sint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[4])) & _uint) == _uint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[5])) & _sint) == _sint);
     free(_fields);
 
     crefl_struct_fields(_types[1], NULL, &nfields);
@@ -56,6 +73,12 @@ void t3()
     assert(_fields);
     crefl_struct_fields(_types[1], _fields, &nfields);
     assert(nfields == 6);
+    assert(crefl_is_field(_fields[0]));
+    assert(crefl_is_field(_fields[1]));
+    assert(crefl_is_field(_fields[2]));
+    assert(crefl_is_field(_fields[3]));
+    assert(crefl_is_field(_fields[4]));
+    assert(crefl_is_field(_fields[5]));
     assert(crefl_decl_tag(_fields[0]) == _decl_field);
     assert(crefl_decl_tag(_fields[1]) == _decl_field);
     assert(crefl_decl_tag(_fields[2]) == _decl_field);
@@ -68,6 +91,18 @@ void t3()
     assert(strcmp(crefl_decl_name(_fields[3]), "f") == 0);
     assert(strcmp(crefl_decl_name(_fields[4]), "g") == 0);
     assert(strcmp(crefl_decl_name(_fields[5]), "p") == 0);
+    assert(crefl_type_width(crefl_field_type(_fields[0])) == 32);
+    assert(crefl_type_width(crefl_field_type(_fields[1])) == 64);
+    assert(crefl_type_width(crefl_field_type(_fields[2])) == 64);
+    assert(crefl_type_width(crefl_field_type(_fields[3])) == 32);
+    assert(crefl_type_width(crefl_field_type(_fields[4])) == 64);
+    assert(crefl_type_width(crefl_field_type(_fields[5])) == 64);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[0])) & _uint) == _uint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[1])) & _sint) == _sint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[2])) & _uint) == _uint);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[3])) & _float) == _float);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[4])) & _float) == _float);
+    assert((crefl_decl_attrs(crefl_field_type(_fields[5])) & _void) == _void);
     free(_fields);
 
     free(_types);
