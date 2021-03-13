fa1cfdd77369703064545bd574ac46b47e3a5962
Michael Clark <michaeljclark@mac.com>
2021-03-13 14:33:41 +1300

    test-cases: add run_tests.py --cpp flag that invokes clang++ -xc++

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -4,6 +4,7 @@ import os
 import glob
 import argparse
 import subprocess
+import itertools
 
 def crefl_source(hdr):
     return hdr
@@ -11,37 +12,41 @@ def crefl_source(hdr):
 def crefl_file(hdr):
     return 'build/tmp/%s.refl' % (os.path.basename(hdr))
 
-def crefl_debug_cmd(hdr):
-    return [
-        'clang', crefl_source(hdr),
-        '-Xclang', '-load',
-        '-Xclang', 'build/libcrefl.so',
-        '-Xclang', '-plugin', '-Xclang', 'crefl',
-        '-Xclang', '-plugin-arg-crefl', '-Xclang', '-debug'
-    ]
+def xclang_args(args):
+    return list(itertools.chain(*zip([ '-Xclang' ] * len(args), args)))
 
-def crefl_meta_cmd(hdr):
-    return [
-        'clang', crefl_source(hdr),
-        '-Xclang', '-load',
-        '-Xclang', 'build/libcrefl.so',
-        '-Xclang', '-plugin', '-Xclang', 'crefl',
-        '-Xclang', '-plugin-arg-crefl', '-Xclang', '-o',
-        '-Xclang', '-plugin-arg-crefl', '-Xclang', crefl_file(hdr)
-    ]
+def xplugin_arg(arg):
+    return xclang_args(['-plugin-arg-crefl', arg ])
+
+def xclang_cmd(hdr, is_cpp):
+    cmd = [ 'clang++', '-c', '-xc++' ] if is_cpp else [ 'clang', '-c' ]
+    cmd += [ crefl_source(hdr) ]
+    cmd += xclang_args(['-load', 'build/libcrefl.so', '-plugin', 'crefl'])
+    return cmd
+
+def crefl_debug_cmd(hdr, is_cpp):
+    cmd = xclang_cmd(hdr, is_cpp)
+    cmd += xplugin_arg('-debug')
+    return cmd
+
+def crefl_meta_cmd(hdr, is_cpp):
+    cmd = xclang_cmd(hdr, is_cpp)
+    cmd += xplugin_arg('-o')
+    cmd += xplugin_arg(crefl_file(hdr))
+    return cmd
 
 def crefl_cat(hdr):
     with open (crefl_source(hdr), "r") as f:
         data = f.read()
         print(data)
 
-def crefl_meta(hdr):
-    cmd = crefl_meta_cmd(hdr)
-    return subprocess.check_output(cmd)
+def crefl_meta(hdr, is_cpp):
+    cmd = crefl_meta_cmd(hdr, is_cpp)
+    return subprocess.run(cmd, check=True)
 
-def crefl_debug(hdr):
-    cmd = crefl_debug_cmd(hdr)
-    return subprocess.check_output(cmd)
+def crefl_debug(hdr, is_cpp):
+    cmd = crefl_debug_cmd(hdr, is_cpp)
+    return subprocess.run(cmd, check=True)
 
 def crefl_dump(hdr):
     cmd = [ './build/crefltool', "--dump", crefl_file(hdr)]
@@ -52,6 +57,8 @@ def crefl_stats(hdr):
     out = subprocess.run(cmd)
 
 parser = argparse.ArgumentParser(description='runs crefl clang plugin on test cases')
+parser.add_argument('--cpp', default=False, action='store_true',
+                    help='enable c++ mode')
 parser.add_argument('--debug', default=False, action='store_true',
                     help='enable crefl debug output')
 parser.add_argument('--stats', default=False, action='store_true',
@@ -70,9 +77,9 @@ for f in args.files:
         crefl_cat(hdr)
         if args.debug:
             print("===== DEBUG test-case: %s =====\n" % (hdr))
-            crefl_debug(hdr)
+            crefl_debug(hdr, args.cpp)
         print("===== OUTPUT test-case: %s =====\n" % (hdr))
-        crefl_meta(hdr)
+        crefl_meta(hdr, args.cpp)
         crefl_dump(hdr)
         if args.stats:
             print("===== STATS test-case: %s =====\n" % (hdr))
