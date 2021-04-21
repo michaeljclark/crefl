c5c00d77a763064ff3074bf329de68216b73129e
Michael Clark <michaeljclark@mac.com>
2021-04-22 09:58:03 +1200

    creflcc.py: improve and rename clang wrapper command output option

--- scripts/creflcc.py
+++ scripts/creflcc.py
@@ -22,17 +22,30 @@ def crefl_meta_cmd(sources, output, is_cpp):
     cmd += sources
     return cmd
 
-def crefl_meta(sources, output, is_cpp, is_verbose):
-    cmd = crefl_meta_cmd(sources, output, is_cpp)
-    if is_verbose:
-        print(" ".join(cmd))
-    return subprocess.run(cmd, check=True)
+def format_cmd(cmd):
+    str = ""
+    lines = []
+    for comp in cmd:
+        if len(str + comp) > 72:
+            lines.append(str)
+            str = ""
+        str += " " + comp if len(str) > 0 else comp
+    if len(str) > 0:
+        lines.append(str)
+    return " \\\n    ".join(lines)
 
-parser = argparse.ArgumentParser(description='runs crefl clang plugin on test cases')
+def crefl_meta(sources, output, is_cpp, no_exec):
+    cmd = crefl_meta_cmd(sources, output, is_cpp)
+    if no_exec:
+        print(format_cmd(cmd))
+    else:
+        return subprocess.run(cmd, check=True)
+
+parser = argparse.ArgumentParser(description='invoke crefl clang plugin')
+parser.add_argument('-n', '--no-exec', default=False, action='store_true',
+                    help='show the comand line invocation')
 parser.add_argument('--cpp', default=False, action='store_true',
                     help='enable c++ mode')
-parser.add_argument('-v', '--verbose', default=False, action='store_true',
-                    help='show comand invocation')
 parser.add_argument('-o', '--output', required=True,
                     help='reflection metadata output')
 parser.add_argument('files', nargs='*',
@@ -42,4 +55,4 @@ args = parser.parse_args()
 if len(args.files) == 0:
     parser.error("no input files")
 
-crefl_meta(args.files, args.output, args.cpp, args.verbose)
+crefl_meta(args.files, args.output, args.cpp, args.no_exec)
