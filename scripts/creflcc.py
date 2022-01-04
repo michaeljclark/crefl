c9cab72050a6f08721471bd538d070065779657a
Michael Clark <michaeljclark@mac.com>
2022-01-04 19:51:13 +1300

    creflcc: update script logic to find clang binary and crefl plugin
    - add search path to find llvm and set LD_LIBRARY_PATH
    - add platform specific plugin name function for .dll, .so, .dylib
    - make uniform interface to  construct clang commands

--- scripts/creflcc.py
+++ scripts/creflcc.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 
+import os
+import platform
 import argparse
 import subprocess
 import itertools
+
+bin_path = '/usr/bin'
+search_prefixes = [ '/opt/llvm']
+_platform_lib_prefix = { 'Linux': 'lib', 'Darwin': 'lib', 'Windows': '' }
+_platform_lib_ext = { 'Linux': '.so', 'Darwin': '.dylib', 'Windows': '.dll' }
+
+for prefix in search_prefixes:
+    if os.path.isfile(prefix + '/bin/clang'):
+        bin_path = prefix + '/bin'
+        os.environ['LD_LIBRARY_PATH'] = prefix + '/lib'
+
+def xclang_c():
+    return '%s/clang' % (bin_path)
+
+def xclang_cxx():
+    return '%s/clang++' % (bin_path)
 
 def xclang_args(args):
     return list(itertools.chain(*zip([ '-Xclang' ] * len(args), args)))
@@ -10,16 +28,22 @@ def xclang_args(args):
 def xplugin_arg(arg):
     return xclang_args(['-plugin-arg-crefl', arg ])
 
+def xclang_plugin(plugin, name):
+    sysname = platform.system();
+    return "%s/%s%s%s" % ( plugin, _platform_lib_prefix[sysname], name, _platform_lib_ext[sysname] )
+
 def xclang_cmd(is_cpp, plugin):
-    cmd = [ 'clang++', '-c', '-xc++' ] if is_cpp else [ 'clang', '-c' ]
-    cmd += xclang_args(['-load', '%s/libcrefl.so' % (plugin), '-plugin', 'crefl'])
+    cmd = [ xclang_cxx(), '-c', '-xc++' ] if is_cpp else [ xclang_c(), '-c' ]
+    cmd += xclang_args(['-load', xclang_plugin(plugin, 'crefl'), '-plugin', 'crefl'])
     return cmd
 
-def crefl_meta_cmd(sources, output, includes, is_cpp, plugin):
+def crefl_meta_cmd(sources, output, includes, is_cpp, is_debug, plugin):
     cmd = xclang_cmd(is_cpp, plugin)
     if includes:
         for include in includes:
             cmd += ['-I%s' % (include)]
+    if is_debug:
+        cmd += xplugin_arg('-debug')
     cmd += xplugin_arg('-o')
     cmd += xplugin_arg(output)
     cmd += sources
@@ -37,8 +61,8 @@ def format_cmd(cmd):
         lines.append(str)
     return " \\\n    ".join(lines)
 
-def crefl_meta(sources, output, includes, is_cpp, plugin, no_exec):
-    cmd = crefl_meta_cmd(sources, output, includes, is_cpp, plugin)
+def crefl_meta(sources, output, includes, is_cpp, is_debug, plugin, no_exec):
+    cmd = crefl_meta_cmd(sources, output, includes, is_cpp, is_debug, plugin)
     if no_exec:
         print(format_cmd(cmd))
     else:
@@ -55,6 +79,8 @@ parser.add_argument('-o', '--output', required=True,
                     help='reflection metadata output')
 parser.add_argument('-p', '--plugin', action='store', default='build',
                     help='directory containing plugin')
+parser.add_argument('-d', '--debug', default=False, action='store_true',
+                    help='enable crefl debug output')
 parser.add_argument('files', nargs='*',
                     help='files to be processed')
 args = parser.parse_args()
@@ -62,4 +88,4 @@ args = parser.parse_args()
 if len(args.files) == 0:
     parser.error("no input files")
 
-crefl_meta(args.files, args.output, args.include, args.cpp, args.plugin, args.no_exec)
+crefl_meta(args.files, args.output, args.include, args.cpp, args.debug, args.plugin, args.no_exec)
--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 
 import os
+import os.path
 import glob
+import platform
 import argparse
 import subprocess
 import itertools
 
-def crefl_source(hdr):
-    return hdr
+bin_path = '/usr/bin'
+search_prefixes = [ '/opt/llvm']
+_platform_lib_prefix = { 'Linux': 'lib', 'Darwin': 'lib', 'Windows': '' }
+_platform_lib_ext = { 'Linux': '.so', 'Darwin': '.dylib', 'Windows': '.dll' }
 
-def crefl_file(hdr):
-    return 'build/tmp/%s.refl' % (os.path.basename(hdr))
+for prefix in search_prefixes:
+    if os.path.isfile(prefix + '/bin/clang'):
+        bin_path = prefix + '/bin'
+        os.environ['LD_LIBRARY_PATH'] = prefix + '/lib'
+
+def xclang_c():
+    return '%s/clang' % (bin_path)
+
+def xclang_cxx():
+    return '%s/clang++' % (bin_path)
 
 def xclang_args(args):
     return list(itertools.chain(*zip([ '-Xclang' ] * len(args), args)))
@@ -18,27 +30,31 @@ def xclang_args(args):
 def xplugin_arg(arg):
     return xclang_args(['-plugin-arg-crefl', arg ])
 
-def xclang_cmd(hdr, is_cpp):
-    cmd = [ 'clang++', '-c', '-xc++' ] if is_cpp else [ 'clang', '-c' ]
-    cmd += [ crefl_source(hdr) ]
-    cmd += xclang_args(['-load', 'build/libcrefl.so', '-plugin', 'crefl'])
+def xclang_plugin(plugin, name):
+    sysname = platform.system();
+    return "%s/%s%s%s" % ( plugin, _platform_lib_prefix[sysname], name, _platform_lib_ext[sysname] )
+
+def xclang_cmd(is_cpp, plugin):
+    cmd = [ xclang_cxx(), '-c', '-xc++' ] if is_cpp else [ xclang_c(), '-c' ]
+    cmd += xclang_args(['-load', xclang_plugin(plugin, 'crefl'), '-plugin', 'crefl'])
     return cmd
 
-def crefl_debug_cmd(hdr, includes, is_cpp):
-    cmd = xclang_cmd(hdr, is_cpp)
+def crefl_source(hdr):
+    return hdr
+
+def crefl_file(hdr):
+    return 'build/tmp/%s.refl' % (os.path.basename(hdr))
+
+def crefl_meta_cmd(hdr, includes, is_cpp, is_debug, plugin):
+    cmd = xclang_cmd(is_cpp, plugin)
     if includes:
         for include in includes:
             cmd += ['-I%s' % (include)]
-    cmd += xplugin_arg('-debug')
-    return cmd
-
-def crefl_meta_cmd(hdr, includes, is_cpp):
-    cmd = xclang_cmd(hdr, is_cpp)
-    if includes:
-        for include in includes:
-            cmd += ['-I%s' % (include)]
+    if is_debug:
+        cmd += xplugin_arg('-debug')
     cmd += xplugin_arg('-o')
     cmd += xplugin_arg(crefl_file(hdr))
+    cmd += [ hdr ]
     return cmd
 
 def crefl_cat(hdr):
@@ -46,13 +62,24 @@ def crefl_cat(hdr):
         data = f.read()
         print(data)
 
-def crefl_meta(hdr, includes, is_cpp):
-    cmd = crefl_meta_cmd(hdr, includes, is_cpp)
-    return subprocess.run(cmd, check=True)
+def format_cmd(cmd):
+    str = ""
+    lines = []
+    for comp in cmd:
+        if len(str + comp) > 72:
+            lines.append(str)
+            str = ""
+        str += " " + comp if len(str) > 0 else comp
+    if len(str) > 0:
+        lines.append(str)
+    return " \\\n    ".join(lines)
 
-def crefl_debug(hdr, includes, is_cpp):
-    cmd = crefl_debug_cmd(hdr, includes, is_cpp)
-    return subprocess.run(cmd, check=True)
+def crefl_meta(hdr, includes, is_cpp, is_debug, no_exec, plugin):
+    cmd = crefl_meta_cmd(hdr, includes, is_cpp, is_debug, plugin)
+    if no_exec:
+        print(format_cmd(cmd))
+    else:
+        return subprocess.run(cmd, check=True)
 
 def crefl_dump(hdr):
     cmd = [ './build/crefltool', "--dump", crefl_file(hdr)]
@@ -78,10 +105,14 @@ def crefl_header(lab, hdr):
     print("===== %-6s test-case: %s =====\n" % (lab, hdr))
 
 parser = argparse.ArgumentParser(description='runs crefl clang plugin on test cases')
+parser.add_argument('-n', '--no-exec', default=False, action='store_true',
+                    help='show the comand line invocation')
 parser.add_argument('--cpp', default=False, action='store_true',
                     help='enable c++ mode')
 parser.add_argument('-I', '--include', action='append',
                     help='include directory')
+parser.add_argument('-p', '--plugin', action='store', default='build',
+                    help='directory containing plugin')
 parser.add_argument('--dump', default=True, action='store_true',
                     help='include standard fields in dump')
 parser.add_argument('--dump-fqn', default=False, action='store_true',
@@ -90,7 +121,7 @@ parser.add_argument('--dump-all', default=False, action='store_true',
                     help='include all fields in dump')
 parser.add_argument('--dump-ext', default=False, action='store_true',
                     help='include all fields in dump')
-parser.add_argument('--debug', default=False, action='store_true',
+parser.add_argument('-d', '--debug', default=False, action='store_true',
                     help='enable crefl debug output')
 parser.add_argument('--stats', default=False, action='store_true',
                     help='enable crefl stats output')
@@ -107,11 +138,10 @@ for f in args.files:
     for hdr in g:
         crefl_header('INPUT', hdr)
         crefl_cat(hdr)
-        if args.debug:
-            crefl_header('DEBUG', hdr)
-            crefl_debug(hdr, args.include, args.cpp)
         crefl_header('OUTPUT', hdr)
-        crefl_meta(hdr, args.include, args.cpp)
+        crefl_meta(hdr, args.include, args.cpp, args.debug, args.no_exec, args.plugin)
+        if args.no_exec:
+            exit(0)
         if args.dump_ext:
             crefl_dump_ext(hdr)
         elif args.dump_fqn:
